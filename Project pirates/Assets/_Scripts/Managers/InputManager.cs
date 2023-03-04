f9b75fa8bc65e314153ccdc40dbad6029139cc13
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-03-04 18:11:43 +0100

    basic camera control

---
+++ Project pirates/Assets/_Scripts/Managers/InputManager.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+    private PlayerInput _playerInput;
+    public static bool IsKeyboardAndMouse => Instance._playerInput.currentControlScheme != "Gamepad";
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        _playerInput = GetComponent<PlayerInput>();
+        SubscribeToInput();
+    }
+
+    public static event Action<CallbackContext> OnLook;
+    public static event Action<CallbackContext> OnMove;
+    public static event Action<CallbackContext> OnInteract;
+
+    private void OnLookInput(CallbackContext context)
+    {
+        OnLook?.Invoke(context);
+    }
+
+    private void OnMoveInput(CallbackContext context)
+    {
+        OnMove?.Invoke(context);
+    }
+
+    private void OnInteractInput(CallbackContext context)
+    {
+        OnInteract?.Invoke(context);
+    }
+
+    private void SubscribeToInput()
+    {
+        _playerInput.actions["Look"].started += OnLookInput;
+        _playerInput.actions["Look"].performed += OnLookInput;
+        _playerInput.actions["Look"].canceled += OnLookInput;
+
+        _playerInput.actions["Move"].started += OnMoveInput;
+        _playerInput.actions["Move"].performed += OnMoveInput;
+        _playerInput.actions["Move"].canceled += OnMoveInput;
+
+        _playerInput.actions["Interact"].started += OnInteractInput;
+        _playerInput.actions["Interact"].performed += OnInteractInput;
+        _playerInput.actions["Interact"].canceled += OnInteractInput;
+    }
+
+    private void UnsubscribeFromInput()
+    {
+        _playerInput.actions["Look"].started -= OnLookInput;
+        _playerInput.actions["Look"].performed -= OnLookInput;
+        _playerInput.actions["Look"].canceled -= OnLookInput;
+
+        _playerInput.actions["Move"].started -= OnMoveInput;
+        _playerInput.actions["Move"].performed -= OnMoveInput;
+        _playerInput.actions["Move"].canceled -= OnMoveInput;
+
+        _playerInput.actions["Interact"].started -= OnInteractInput;
+        _playerInput.actions["Interact"].performed -= OnInteractInput;
+        _playerInput.actions["Interact"].canceled -= OnInteractInput;
+    }
+    private void OnDestroy()
+    {
+        UnsubscribeFromInput();
+    }
+
+    [ContextMenu("Debug print controlscheme")]
+    private void DebugPrintControlscheme()
+    {
+        Debug.Log("Is keyboard and mouse: " + IsKeyboardAndMouse);
+    }
+}
---
+++ Project pirates/Assets/_Scripts/_ScriptableObjectScripts/PlayerSettings.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSettings", menuName = "Project pirates/PlayerSettings", order = 0)]
+public class PlayerSettings : ScriptableObject
+{
+    [field: Header("General")]
+    [field: SerializeField] public bool CloneScriptableObjectOnStart { get; private set; } = true;
+    [field: Header("Volume")]
+    [field: SerializeField] public float MasterVolume { get; private set; } = 0.3f;
+    [field: SerializeField] public float MusicVolume { get; private set; } = 1f;
+    [field: SerializeField] public float SfxVolume { get; private set; } = 1f;
+    [field: Header("Controls")]
+    [field: SerializeField] public float MouseLookSensitivity { get; private set; } = 1f;
+    [field: SerializeField] public float GamepadLookSensitivity { get; private set; } = 1f;
+    [field: SerializeField] public bool InvertYAxis { get; private set; } = false;
+    [field: Header("Movement")]
+    [field: SerializeField] public float MovementSpeed { get; private set; } = 1f;
+    [field: SerializeField][field: Range(0.01f,1f)] public float MovementSnapSpeed { get; private set; } = 1f;
+    [field: Header("Camera")]
+    [field: SerializeField] public float CameraMinX { get; private set; } = 1f;
+    [field: SerializeField] public float CameraMaxX { get; private set; } = 1f;
+
+    [HideInInspector] public event Action<string> OnSettingChanged;
+
+}
