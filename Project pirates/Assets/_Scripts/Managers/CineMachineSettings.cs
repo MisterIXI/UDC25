54b943e35a3f4a399f74dd91edbc08c8f6bf4097
Robert Moegenburg <r.moegenburg@t-online.de>
2023-03-04 20:15:14 +0100

    cinemaschineExtension

---
+++ Project pirates/Assets/_Scripts/Managers/CineMachineSettings.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CineMachineSettings : CinemachineExtension
+{
+    private PlayerController playercontroller;
+    private Vector3 startingRotation; // ROTATION
+    [SerializeField] private float horizontalSpeed = 10f;
+    [SerializeField] private float verticalSpeed = 10f;
+    [SerializeField] private float clampAngle = 80f;
+    protected override void Awake()
+    {
+        playercontroller = PlayerController.Instance;
+        base.Awake();
+    }
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (vcam.Follow)
+        {
+            if(stage == CinemachineCore.Stage.Aim)
+            {
+                // Debug.Log(playercontroller.GetDelta());
+                if (startingRotation == null) startingRotation = transform.localRotation.eulerAngles;
+                Vector2 deltaInput = playercontroller.GetDelta();
+                startingRotation.x += deltaInput.x * verticalSpeed * Time.deltaTime;
+                startingRotation.y += deltaInput.y * horizontalSpeed * Time.deltaTime;
+                startingRotation.y = Mathf.Clamp(startingRotation.y, -clampAngle, -clampAngle);
+                state.RawOrientation = Quaternion.Euler(-startingRotation.y, startingRotation.x, 0f);
+            }
+        }
+    }
+}
