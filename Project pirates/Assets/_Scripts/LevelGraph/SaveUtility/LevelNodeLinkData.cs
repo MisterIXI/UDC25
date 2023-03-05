49386ce20128ec982e82e1b7d4815b9f07304268
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-03-05 19:17:52 +0100

    Node Progress

---
+++ Project pirates/Assets/_Scripts/LevelGraph/SaveUtility/LevelNodeLinkData.cs
@@ -0,0 +1,9 @@
+using System;
+[Serializable]
+public class LevelNodeLinkData
+{
+    public string BaseNodeGUID;
+    public string BasePortName;
+    public string TargetNodeGUID;
+    public string TargetPortName;
+}
