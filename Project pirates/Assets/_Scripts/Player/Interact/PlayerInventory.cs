b5af6a5257f0d2c1d32315a308a5329c17f0bc97
OliverPohl <oliver@pohl66.de>
2023-03-05 20:33:41 +0100

    layer added

--- Project pirates/Assets/_Scripts/Player/Interact/PlayerInventory.cs
+++ Project pirates/Assets/_Scripts/Player/Interact/PlayerInventory.cs
@@ -13,11 +13,13 @@ public class PlayerInventory : MonoBehaviour
         {
             trans.SetParent(null);
             trans.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            trans.gameObject.layer = 0;
             trans.position = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 2));
         }
 
         newObj.transform.SetParent(inventory.transform);
         newObj.GetComponent<Rigidbody>().isKinematic = true;
+        newObj.layer = 5;
 
         newObj.transform.localPosition = new Vector3 (0,0,0);
         newObj.transform.localRotation = Quaternion.Euler(0,4,4);
