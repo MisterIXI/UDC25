52c9dac3ed3a8b934a671718de01e49d4e7b7641
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-03-05 12:12:40 +0100

    UI Progress

--- Project pirates/Assets/_Scripts/Managers/GameManager.cs
+++ Project pirates/Assets/_Scripts/Managers/GameManager.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance { get; private set; }
+    public static Gamestate CurrentGameState { get; private set; }
+    public static event Action<Gamestate, Gamestate> OnGameStateChanged;
+    public static bool IsGameActiveAndPlaying => CurrentGameState == Gamestate.InGame;
+    [field: SerializeField] public bool StartInMenu { get; private set; } = false;
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        OnGameStateChanged += OnGameStateChange;
+    }
+    private void Start()
+    {
+        if (StartInMenu)
+            SwitchToGameState(Gamestate.MainMenu);
+        else
+            SwitchToGameState(Gamestate.InGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void SwitchToGameState(Gamestate newState)
     {
-        
+        OnGameStateChanged?.Invoke(CurrentGameState, newState);
+        CurrentGameState = newState;
     }
+    private void OnGameStateChange(Gamestate oldState, Gamestate newState)
+    {
+        // timescale
+        switch (newState)
+        {
+            case Gamestate.InGame:
+                Time.timeScale = 1;
+                break;
+            case Gamestate.MainMenu:
+            case Gamestate.Paused:
+            case Gamestate.GameOver:
+                Time.timeScale = 0;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+    }
+}
+
+public enum Gamestate
+{
+    MainMenu,
+    InGame,
+    Paused,
+    GameOver
 }
--- Project pirates/Assets/_Scripts/Menu/MenuManager.cs
+++ Project pirates/Assets/_Scripts/Menu/MenuManager.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     public static MenuManager Instance { get; private set; }
+    [field: SerializeField] private MenuBase _menuMain { get; set; }
+    [field: SerializeField] private MenuBase _menuSettings { get; set; }
+    [field: SerializeField] private MenuBase _menuCredits { get; set; }
+    [field: SerializeField] private MenuBase _menuPause { get; set; }
+    [field: SerializeField] private MenuBase _menuHUD { get; set; }
+    private static MenuType _currentMenu;
+    private static MenuType _previousMenu;
 
     private void Awake()
     {
@@ -15,4 +22,73 @@ public class MenuManager : MonoBehaviour
         }
         Instance = this;
     }
+    private void Start()
+    {
+        InitializeMenus();
+        ShowMenu(MenuType.Main);
+    }
+    private void InitializeMenus()
+    {
+        _menuMain.Initialize();
+        _menuSettings.Initialize();
+        _menuCredits.Initialize();
+        _menuPause.Initialize();
+        _menuHUD.Initialize();
+    }
+    public static void ShowMenu(MenuType menu)
+    {
+        Instance.SetActiveOnMenu(_currentMenu, false);
+        if (Instance.SetActiveOnMenu(menu, true))
+        {
+            _previousMenu = _currentMenu;
+            _currentMenu = menu;
+        }
+        else // fall back to previous menu
+            Instance.SetActiveOnMenu(_currentMenu, true);
+    }
+
+    public static void ShowPreviousMenu()
+    {
+        ShowMenu(_previousMenu);
+    }
+
+    private bool SetActiveOnMenu(MenuType menu, bool active)
+    {
+        switch (menu)
+        {
+            case MenuType.Main:
+                _menuMain.gameObject.SetActive(active);
+                _menuMain.SetSelection();
+                break;
+            case MenuType.Settings:
+                _menuSettings.gameObject.SetActive(active);
+                _menuSettings.SetSelection();
+                break;
+            case MenuType.Credits:
+                _menuCredits.gameObject.SetActive(active);
+                _menuCredits.SetSelection();
+                break;
+            case MenuType.Pause:
+                _menuPause.gameObject.SetActive(active);
+                _menuPause.SetSelection();
+                break;
+            case MenuType.HUD:
+                _menuHUD.gameObject.SetActive(active);
+                _menuHUD.SetSelection();
+                break;
+            default:
+                Debug.LogError("MenuManager.SetActiveOnMenu: Invalid menu type");
+                return false;
+        }
+        return true;
+    }
+}
+
+public enum MenuType
+{
+    Main,
+    Settings,
+    Credits,
+    Pause,
+    HUD
 }
