1644eff86ddb93004f44193261beed0f97aeff4a
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-03-07 03:03:55 +0100

    progress commit

--- Project pirates/Assets/_Scripts/CustomEditors/AnchorListEditor.cs
+++ Project pirates/Assets/_Scripts/CustomEditors/AnchorListEditor.cs
@@ -6,10 +6,6 @@ public class AnchorListEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        if (GUILayout.Button("Generate GUID"))
-        {
-            ((AnchorList)target).GUID = System.Guid.NewGuid().ToString();
-        }
         base.OnInspectorGUI();
     }
 }
