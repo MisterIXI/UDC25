b5b775245b075cdcdb8e9b27c7f9c421e5d1bca6
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-03-07 17:59:19 +0100

    LevelNode rotation added; Edge reconnection added

--- Project pirates/Assets/_Scripts/LevelGraph/LevelGraphView.cs
+++ Project pirates/Assets/_Scripts/LevelGraph/LevelGraphView.cs
@@ -17,10 +17,27 @@ public class LevelGraphView : GraphView
         this.AddManipulator(new ContentDragger());
         this.AddManipulator(new SelectionDragger());
         this.AddManipulator(new RectangleSelector());
-
+        // viewTransformChanged += test;
         CreateGridBackground();
     }
+    private Vector2 viewPortCenter = Vector2.zero;
+    // private Vector2 viewPortCenter => -new Vector2(viewTransform.matrix[0, 3], viewTransform.matrix[1, 3]) - (layout.size / 2);
+    // private void test(GraphView s)
+    // {
+    //     Debug.Log(viewTransform);
+    //     // Debug.Log(layout.size);
+    //     Debug.Log(new Vector2(viewTransform.matrix[0, 3], viewTransform.matrix[1, 3]));
+    //     Debug.Log(layout.size);
+    //     Debug.Log(viewPortCenter);
+    //     // Debug.Log("position : " + viewTransform.position);
+    //     // Debug.Log("scale : " + contentViewContainer.worldBound);
 
+    //     // Debug.Log($"Layout: {layout.size}");
+    //     // Debug.Log($"LocalBound: {localBound}");
+    //     // Debug.Log($"WorldBound: {worldBound}");
+    //     // Debug.Log($"ContentRect: {contentRect}");
+    //     // Debug.Log($"PaddingRect: {paddingRect}");
+    // }
     private Port GeneratePort(Node node, Direction portDirection, Port.Capacity capacity = Port.Capacity.Single)
     {
         return node.InstantiatePort(Orientation.Horizontal, portDirection, capacity, typeof(float));
@@ -37,15 +54,17 @@ public class LevelGraphView : GraphView
     {
         AddElement(CreateLinkNode("Link"));
     }
-    public LevelNode CreateAndAddLevelNode(string nodeName, string GUID, Rect position, AnchorList anchorList)
+    public LevelNode CreateAndAddLevelNode(string nodeName, string GUID, Rect position, AnchorList anchorList, float YRotation)
     {
         var levelNode = CreateLevelNode(nodeName, GUID);
         levelNode.SetPosition(position);
         UpdateOutputCount(levelNode, anchorList);
         levelNode.anchorList = anchorList;
+        levelNode.YRotation = YRotation;
         levelNode.RefreshExpandedState();
         levelNode.RefreshPorts();
         levelNode.mainContainer.Q<ObjectField>().value = anchorList;
+        levelNode.mainContainer.Q<FloatField>().value = YRotation;
         AddElement(levelNode);
         return levelNode;
     }
@@ -96,10 +115,20 @@ public class LevelGraphView : GraphView
             UpdateOutputCount(levelNode, levelNode.anchorList);
         });
         levelNode.mainContainer.Add(referenceField);
-
+        var rotationField = new FloatField
+        {
+            name = "YRotation",
+            value = 0,
+            label = "Y Axis Rotation"
+        };
+        rotationField.RegisterValueChangedCallback(evt =>
+        {
+            levelNode.YRotation = evt.newValue;
+        });
+        levelNode.mainContainer.Add(rotationField);
         levelNode.RefreshExpandedState();
         levelNode.RefreshPorts();
-        levelNode.SetPosition(new Rect(Vector2.zero, defaultNodeSize));
+        levelNode.SetPosition(new Rect(viewPortCenter, defaultNodeSize));
         return levelNode;
     }
 
@@ -131,7 +160,7 @@ public class LevelGraphView : GraphView
         decisionNode.outputContainer.Add(outputPortFalse);
         decisionNode.RefreshExpandedState();
         decisionNode.RefreshPorts();
-        decisionNode.SetPosition(new Rect(Vector2.zero, defaultNodeSize));
+        decisionNode.SetPosition(new Rect(viewPortCenter, defaultNodeSize));
         return decisionNode;
     }
 
@@ -173,7 +202,8 @@ public class LevelGraphView : GraphView
         linkNode.outputContainer.Add(outputPort);
         linkNode.RefreshExpandedState();
         linkNode.RefreshPorts();
-        linkNode.SetPosition(new Rect(Vector2.zero, defaultNodeSize));
+
+        linkNode.SetPosition(new Rect(viewPortCenter, defaultNodeSize));
         return linkNode;
     }
     public void CreateGridBackground()
@@ -202,6 +232,18 @@ public class LevelGraphView : GraphView
         if (anchorlist == null)
             return;
         int anchorCount = anchorlist.anchors.Count;
+        List<Edge> edgesOut = new List<Edge>();
+        foreach (Port port in levelNode.outputContainer.Query<Port>().ToList())
+        {
+            if (port.direction == Direction.Output)
+                edgesOut.AddRange(port.connections.ToList());
+        }
+        List<Edge> edgesIn = new List<Edge>();
+        foreach (Port port in levelNode.inputContainer.Query<Port>().ToList())
+        {
+            if (port.direction == Direction.Input)
+                edgesIn.AddRange(port.connections.ToList());
+        }
         levelNode.outputContainer.Clear();
         levelNode.inputContainer.Clear();
         for (int i = 0; i < anchorCount; i++)
@@ -210,10 +252,29 @@ public class LevelGraphView : GraphView
             var outputPort = GeneratePort(levelNode, Direction.Output, Port.Capacity.Multi);
             inputPort.portName = anchorlist.anchors[i].name;
             outputPort.portName = anchorlist.anchors[i].name;
+            // connect fitting input edges
+            foreach (Edge edge in edgesIn.Where(x => x.input.portName == inputPort.portName))
+            {
+                edge.input = inputPort;
+                inputPort.Connect(edge);
+            }
+            edgesIn.RemoveAll(x => x.input.portName == inputPort.portName);
+            // connect fitting output edges
+            foreach (Edge edge in edgesOut.Where(x => x.output.portName == outputPort.portName))
+            {
+                edge.output = outputPort;
+                outputPort.Connect(edge);
+            }
+            edgesOut.RemoveAll(x => x.output.portName == outputPort.portName);
             levelNode.inputContainer.Add(inputPort);
             levelNode.outputContainer.Add(outputPort);
         }
+        foreach (Edge edge in edgesOut)
+            edge.parent.Remove(edge);
+        foreach (Edge edge in edgesIn)
+            edge.parent.Remove(edge);
         levelNode.RefreshPorts();
         levelNode.RefreshExpandedState();
+
     }
 }
