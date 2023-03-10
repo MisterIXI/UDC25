41edd759c9a9b6b47ae21a07354feb430ed2025a
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-03-10 17:05:07 +0100

    PlayerInteract improvements

--- Project pirates/Assets/_Scripts/Player/Interact/PlayerInventory.cs
+++ Project pirates/Assets/_Scripts/Player/Interact/PlayerInventory.cs
@@ -1,50 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
     GameObject inventory;
-    public Vector3 destItemRotation;
-    public float rubberbandForce;
-    public float rotationSpeed;
     private Camera mainCamera;
     private GameObject item;
-
-    private void Start() 
+    private PlayerSettings _playerSettings;
+    private void Start()
     {
+        _playerSettings = SettingsManager.PlayerSettings;
         mainCamera = Camera.main;
         inventory = new GameObject("Inventory");
         inventory.tag = "Inventory";
         inventory.transform.SetParent(mainCamera.transform);
-        inventory.transform.localPosition = new Vector3(-0.5f, -0.275f, 1); //change this for size and position of items
+        inventory.transform.localPosition = _playerSettings.InvItemPosition;
     }
 
 
-    private void FixedUpdate() 
+    private void FixedUpdate()
     {
         if (item != null)
         {
             MoveObject();
-            Vector3 startRot = item.transform.localEulerAngles;   
-                    Vector3 currentAngle = new Vector3(
-            Mathf.LerpAngle(startRot.x, destItemRotation.x, Time.fixedDeltaTime * rotationSpeed),
-            Mathf.LerpAngle(startRot.y, destItemRotation.y, Time.fixedDeltaTime * rotationSpeed),
-            Mathf.LerpAngle(startRot.z, destItemRotation.z, Time.fixedDeltaTime * rotationSpeed));
+            Vector3 startRot = item.transform.localEulerAngles;
+            Vector3 currentAngle = new Vector3(
+    Mathf.LerpAngle(startRot.x, _playerSettings.InvItemRotation.x, Time.fixedDeltaTime * _playerSettings.InvRotationSpeed),
+    Mathf.LerpAngle(startRot.y, _playerSettings.InvItemRotation.y, Time.fixedDeltaTime * _playerSettings.InvRotationSpeed),
+    Mathf.LerpAngle(startRot.z, _playerSettings.InvItemRotation.z, Time.fixedDeltaTime * _playerSettings.InvRotationSpeed));
             item.transform.localEulerAngles = currentAngle;
         }
     }
 
     private void MoveObject()
     {
-        if (Vector3.Distance(item.transform.position, inventory.transform.position) > 0.1f)
-        { 
+        if (Vector3.Distance(item.transform.position, inventory.transform.position) > 0.01f)
+        {
             Vector3 moveDirection = (inventory.transform.position - item.transform.position);
-            item.GetComponent<Rigidbody>().AddForce(moveDirection * rubberbandForce);
+            item.GetComponent<Rigidbody>().AddForce(moveDirection * _playerSettings.InvRubberbandForce);
         }
 
-        if(Vector3.Distance(item.transform.position, inventory.transform.position) > 5f)
+        if (Vector3.Distance(item.transform.position, inventory.transform.position) > 5f)
         {
             item.transform.position = inventory.transform.position;
         }
@@ -53,17 +51,24 @@ public class PlayerInventory : MonoBehaviour
     public void TakeObject(GameObject newObj)
     {
         item = newObj;
-
-        foreach  (Transform trans in inventory.transform)
+        foreach (Transform trans in inventory.transform)
         {
             trans.gameObject.GetComponent<Collider>().enabled = true;
+            Rigidbody transRB = trans.GetComponent<Rigidbody>();
+            UpdateRigidbody(transRB, false);
+            transRB.AddForce(mainCamera.transform.forward * _playerSettings.InteractThrowMagnitude, ForceMode.Impulse);
             // trans.position = item.transform.position;
             trans.SetParent(null);
         }
-        newObj.GetComponent<Rigidbody>().useGravity = false;
-        newObj.GetComponent<Rigidbody>().drag = 10;
-        newObj.GetComponent<Rigidbody>().angularDrag = 3;
+        UpdateRigidbody(item.GetComponent<Rigidbody>(), true);
         item.transform.SetParent(inventory.transform);
+    }
+
+    private void UpdateRigidbody(Rigidbody rb, bool value)
+    {
+        rb.useGravity = !value;
+        rb.drag = value ? 10 : 1;
+        rb.angularDrag = value ? 3 : 0.05f;
     }
 
 }
