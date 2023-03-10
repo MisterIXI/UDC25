fa8934d3e141a18d766b703671e30bb6b8d97fa4
OliverPohl <oliver@pohl66.de>
2023-03-10 19:54:00 +0100

    Pult interact finished

---
+++ Project pirates/Assets/_Scripts/Compass/FogOn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOn : MonoBehaviour
+{
+    private PlayerSettings _playerSettings;
+
+
+    void Start()
+    {
+        _playerSettings = SettingsManager.PlayerSettings;
+        RenderSettings.fog = true;
+        RenderSettings.fogDensity = 0f;
+    }
+
+
+    void FixedUpdate()
+    {
+        if (RenderSettings.fogDensity != _playerSettings.FogDensity)
+        {
+            RenderSettings.fogDensity = Mathf.Lerp(RenderSettings.fogDensity, _playerSettings.FogDensity, Time.deltaTime);
+        }
+    }
+}
+ 
