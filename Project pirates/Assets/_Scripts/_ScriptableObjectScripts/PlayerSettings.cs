44485b6c092d53559e64f2caffa88d42e2c74f75
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-03-10 12:26:36 +0100

    player controller snap implementation

--- Project pirates/Assets/_Scripts/_ScriptableObjectScripts/PlayerSettings.cs
+++ Project pirates/Assets/_Scripts/_ScriptableObjectScripts/PlayerSettings.cs
@@ -19,11 +19,11 @@ public class PlayerSettings : ScriptableObject
     [field: SerializeField] public float SensitivityMultiplier { get; set; } = 10f;
     [field: Header("Movement")]
     [field: SerializeField] public float MovementSpeed { get; set; } = 1f;
-    [field: SerializeField][field: Range(0.01f, 1f)] public float MovementSnapSpeed { get; set; } = 1f;
+    [field: SerializeField][field: Range(0.01f, 1f)] public float MovementSnapSeconds { get; set; } = 1f;
     [field: Header("Camera")]
 
     [field: SerializeField][field: Range(0.01f, 89.99f)] public float CameraClampAngleUp { get; set; } = 80f;
-    [field: SerializeField][field: Range(0.01f, 89.99f)] public float CameraClampAngleDown { get; set; } = -80f;
+    [field: SerializeField][field: Range(0.01f, -89.99f)] public float CameraClampAngleDown { get; set; } = -80f;
     [field: Header("HoldObject")]
     [field: SerializeField] public float HoldObjectMaxDistance { get; set; } = 3f;
     [field: SerializeField] public float HoldObjectMaxSpeedMagnitude { get; set; } = 3f;
