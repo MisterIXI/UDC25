18dedae71c00b4ce56a980da546ce160086ad8a6
Robert Moegenburg <r.moegenburg@t-online.de>
2023-03-11 21:49:05 +0100

    coinmec and logoSmall

---
+++ Project pirates/Assets/_Scripts/Player/CoinManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager
+{
+    // INSTANCING TO GAME
+    // PUBLIC COIN CLASS 
+    // GET PRIVATE SET
+    // NEW COIN TO INVENTORY
+    // PUBLIC VOID DROP COIN
+    // SHOW HIDE ALL COINS TO ORBIT AROUND PLAYER
+    // INTERACT WITH TAKE COIN SCRIPT AND INVENTORY
+}
