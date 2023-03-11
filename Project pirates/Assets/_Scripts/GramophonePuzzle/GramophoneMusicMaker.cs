d5699d2f1fca7a246c981b9bcccf25db5549de41
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-03-11 20:35:31 +0100

    Gramophone prototype completed

---
+++ Project pirates/Assets/_Scripts/GramophonePuzzle/GramophoneMusicMaker.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class GramophoneMusicMaker : MonoBehaviour
+{
+
+}
--- Project pirates/Assets/_Scripts/_ScriptableObjectScripts/GramophoneSettings.cs
+++ Project pirates/Assets/_Scripts/_ScriptableObjectScripts/GramophoneSettings.cs
@@ -6,4 +6,7 @@ public class GramophoneSettings : ScriptableObject
     [field: Header("GramophoneFollowNoise")]
     [field: SerializeField] public bool DrawDebugGizmos { get; private set; } = false;
     [field: SerializeField] public float GramoFollowSpeed { get; private set; } = 7f;
+    [field: Header("GramophoneBackwardsNoise")]
+    [field: SerializeField] public float GramoBackwardsNoiseTriggerDuration { get; private set; } = 5f;
+    [field: SerializeField][field: Range(0, 10)] public int GramoBackwardsMinimumNodeChanges { get; private set; } = 3;
 }
