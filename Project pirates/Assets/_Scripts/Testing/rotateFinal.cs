2554aeac8e6a50baed4228c86aaa647d9737d7b3
Robert Moegenburg <r.moegenburg@t-online.de>
2023-03-12 21:18:43 +0100

    decoration03 and itch screenshots

---
+++ Project pirates/Assets/_Scripts/Testing/rotateFinal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateFinal : MonoBehaviour
+{
+    [SerializeField]private float speed= 10f;
+    private void Rotate()
+    {
+        transform.RotateAround(transform.parent.position,Vector3.back,speed*Time.deltaTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+    public void SetSpeed(float value)
+    {
+        speed = value;
+    }
+}
