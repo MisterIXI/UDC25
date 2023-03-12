444322d33ee7e67b659eceeeafca8114aca431fe
Robert Moegenburg <r.moegenburg@t-online.de>
2023-03-12 12:41:56 +0100

    titleCutscene 01

--- Project pirates/Assets/_Scripts/_ScriptableObjectScripts/CoinObject.cs
+++ Project pirates/Assets/_Scripts/_ScriptableObjectScripts/CoinObject.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CoinObject", menuName ="CoinObject")]
 public class CoinObject: ScriptableObject
 {
-    [field: Header("All Coins listed here:")]
-    [field: SerializeField] public List<int> CoinList { get; set; } = new List<int>();
-
+    
     [field: Header("CoinObjects:")]
     [field: SerializeField] public GameObject BronzeObject { get; private set; }
     [field: SerializeField] public GameObject SilverObject { get; private set; }
