060e13570ca3e9479c01574fc562fc472b151563
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-03-12 23:13:21 +0100

    raft scene finished

---
+++ Project pirates/Assets/_Scripts/RaftSceneScripts/RaftRandomRotationImpulse.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class RaftRandomRotationImpulse : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.AddTorque(Random.insideUnitSphere / 2f, ForceMode.Impulse);
+        }
+    }
+}
